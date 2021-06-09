7cfe654900453c1db0db5014d200e88a967b60b2
zyhou <richard.maxime9@gmail.com>
2021-06-09 09:44:41 +0200

    add icon in download button

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,24 @@ export function App(): React.ReactElement {
             <main>
                 <section>
                     <SelectThemes setTheme={setTheme} />
-                    <button onClick={handleOnClick}>Download Wallpaper</button>
+                    <button
+                        onClick={handleOnClick}
+                        className="w-56 flex items-center py-2 px-4 max-w-xs mx-auto text-base font-semibold font-primary rounded-md focus:outline-none focus:ring-1 focus:ring-palette-primary focus:ring-offset-2"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5 mr-2"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                        Download Wallpaper
+                    </button>
                 </section>
                 <section className="flex bg-gray-900 dark:bg-white">
                     <Wallpaper canvasReference={canvasReference} theme={theme} />
