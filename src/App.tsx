c67bd0429dc365d7b8a487bea75890a0e2d5b057
zyhou <richard.maxime9@gmail.com>
2021-06-18 10:55:33 +0200

    refacto background color picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,14 @@
 import React from 'react';
-import { HexColorPicker, HexColorInput } from 'react-colorful';
-import { useLayer, Arrow } from 'react-laag';
-import { colord } from 'colord';
 
 import type { Theme } from './types';
 import { Wallpaper } from './Wallpaper';
 import { ActionButtons } from './ActionButtons';
+import { EditBackgroundColor } from './EditBackgroundColor';
 
 const getTwitterUrl = () =>
     `https://twitter.com/intent/tweet?text=${encodeURIComponent(
         `Thank you @rmaximedev for my new wallpaper 😍 #wallpaper`,
     )}`;
-
-type PropertiesBackgroundColorInput = {
-    theme: Theme;
-    setTheme: React.Dispatch<React.SetStateAction<Theme>>;
-};
-
-function BackgroundColorInput({
-    theme,
-    setTheme,
-}: PropertiesBackgroundColorInput): React.ReactElement {
-    const [isOpen, setOpen] = React.useState<boolean>(false);
-    function close() {
-        setOpen(false);
-    }
-
-    const { renderLayer, triggerProps, layerProps, arrowProps } = useLayer({
-        isOpen,
-        onOutsideClick: close,
-        onDisappear: close,
-        overflowContainer: false,
-        placement: 'bottom-center',
-        triggerOffset: 10,
-    });
-
-    const onChangeColor = (color: string) => {
-        setTheme((currentTheme) => ({
-            ...currentTheme,
-            background: color,
-            isDark: colord(color).isDark(),
-        }));
-    };
-
-    return (
-        <React.Fragment>
-            <button
-                className="swatch"
-                style={{ backgroundColor: theme.background }}
-                onClick={() => setOpen((open) => !open)}
-                {...triggerProps}
-            />
-            {renderLayer(
-                <div>
-                    {isOpen && (
-                        <div {...layerProps}>
-                            <HexColorPicker color={theme.background} onChange={onChangeColor} />
-                            <div className="p-4 bg-gray-900 text-white rounded-b-lg">
-                                <div className="text-center before:content-['#'] before:absolute before:pointer-events-none before:opacity-50 before:w-6 before:bottom-5">
-                                    <HexColorInput
-                                        color={theme.background}
-                                        onChange={onChangeColor}
-                                        className="w-24 p-1 bg-gray-900 border-gray-700 border rounded text-center"
-                                    />
-                                </div>
-                            </div>
-                            <Arrow {...arrowProps} backgroundColor="rgba(17, 24, 39, 1)" />
-                        </div>
-                    )}
-                </div>,
-            )}
-        </React.Fragment>
-    );
-}
 
 export function App(): React.ReactElement {
     const [theme, setTheme] = React.useState<Theme>({
@@ -139,16 +75,13 @@ export function App(): React.ReactElement {
             </header>
             <main className="flex-grow mt-8">
                 <ActionButtons theme={theme} setTheme={setTheme} />
-                <section className="container flex flex-row justify-center items-center mx-auto mb-2 md:flex-row">
-                    background color:
-                    <BackgroundColorInput theme={theme} setTheme={setTheme} />
-                </section>
+                <EditBackgroundColor theme={theme} setTheme={setTheme} />
                 <section className="relative w-full">
                     <div className="arrow">
                         <img
                             alt="Click to customize Arrow"
                             src="/down-arrow.png"
-                            className={`mr-2 w-10 ${theme.isDark ? `invert` : `invert-0`}`}
+                            className={`mr-2 w-10 filter ${theme.isDark ? `invert` : `invert-0`}`}
                         ></img>
                         <span className="inline-block -translate-y-full">Click to customize</span>
                     </div>
---
+++ src/EditBackgroundColor.tsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import { HexColorPicker, HexColorInput } from 'react-colorful';
+import { useLayer, Arrow } from 'react-laag';
+import { colord } from 'colord';
+
+import type { Theme } from './types';
+
+type PropertiesBackgroundColorInput = {
+    theme: Theme;
+    setTheme: React.Dispatch<React.SetStateAction<Theme>>;
+    isOpen: boolean;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+function BackgroundColorInput({
+    theme,
+    setTheme,
+    isOpen,
+    setOpen,
+}: PropertiesBackgroundColorInput): React.ReactElement {
+    function close() {
+        setOpen(false);
+    }
+
+    const { renderLayer, triggerProps, layerProps, arrowProps } = useLayer({
+        isOpen,
+        onOutsideClick: close,
+        onDisappear: close,
+        overflowContainer: false,
+        placement: 'bottom-center',
+        triggerOffset: 10,
+    });
+
+    const onChangeColor = (color: string) => {
+        setTheme((currentTheme) => ({
+            ...currentTheme,
+            background: color,
+            isDark: colord(color).isDark(),
+        }));
+    };
+
+    return (
+        <React.Fragment>
+            <button
+                className="w-8 h-8 cursor-pointer shadow-md rounded-lg border-2 border-blue-600 hover:border-blue-700 focus:outline-none"
+                style={{ backgroundColor: theme.background }}
+                onClick={() => setOpen((open) => !open)}
+                {...triggerProps}
+            />
+            {renderLayer(
+                <div>
+                    {isOpen && (
+                        <div {...layerProps}>
+                            <HexColorPicker color={theme.background} onChange={onChangeColor} />
+                            <div className="p-4 bg-gray-900 text-white rounded-b-lg">
+                                <div className="text-center before:content-['#'] before:absolute before:pointer-events-none before:opacity-50 before:w-6 before:bottom-5">
+                                    <HexColorInput
+                                        color={theme.background}
+                                        onChange={onChangeColor}
+                                        className="w-24 p-1 bg-gray-900 border-gray-700 border rounded text-center"
+                                    />
+                                </div>
+                            </div>
+                            <Arrow {...arrowProps} backgroundColor="rgba(17, 24, 39, 1)" />
+                        </div>
+                    )}
+                </div>,
+            )}
+        </React.Fragment>
+    );
+}
+
+type Properties = {
+    theme: Theme;
+    setTheme: React.Dispatch<React.SetStateAction<Theme>>;
+};
+
+export function EditBackgroundColor({ theme, setTheme }: Properties): React.ReactElement {
+    const [isOpen, setOpen] = React.useState<boolean>(false);
+
+    return (
+        <section className="container hidden flex-row justify-center items-center mx-auto mb-2 md:flex">
+            <div className="opacity-80 hover:opacity-100 flex items-center">
+                <button
+                    className="cursor-pointer text-lg focus:outline-none"
+                    onClick={() => setOpen(true)}
+                >
+                    Click to edit background
+                </button>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 mx-2"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M17 8l4 4m0 0l-4 4m4-4H3"
+                    />
+                </svg>
+            </div>
+            <BackgroundColorInput
+                theme={theme}
+                setTheme={setTheme}
+                isOpen={isOpen}
+                setOpen={setOpen}
+            />
+        </section>
+    );
+}
